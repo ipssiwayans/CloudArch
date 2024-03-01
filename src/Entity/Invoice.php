b72ca3f6a9af5aee2f127782aadca551b7efa057
samueltomen <113606314+samueltomen@users.noreply.github.com>
2024-03-01 19:48:43 +0100

    init database

---
+++ src/Entity/Invoice.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InvoiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InvoiceRepository::class)]
+class Invoice
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $object = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $amount_ht = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $quantity = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $unit_price_ht = null;
+
+    #[ORM\Column]
+    private ?float $amount_tva = null;
+
+    #[ORM\Column]
+    private ?float $total_amount = null;
+
+    #[ORM\ManyToOne(inversedBy: 'number_invoices')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getObject(): ?string
+    {
+        return $this->object;
+    }
+
+    public function setObject(?string $object): static
+    {
+        $this->object = $object;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAmountHt(): ?float
+    {
+        return $this->amount_ht;
+    }
+
+    public function setAmountHt(float $amount_ht): static
+    {
+        $this->amount_ht = $amount_ht;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(?int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getUnitPriceHt(): ?float
+    {
+        return $this->unit_price_ht;
+    }
+
+    public function setUnitPriceHt(?float $unit_price_ht): static
+    {
+        $this->unit_price_ht = $unit_price_ht;
+
+        return $this;
+    }
+
+    public function getAmountTva(): ?float
+    {
+        return $this->amount_tva;
+    }
+
+    public function setAmountTva(float $amount_tva): static
+    {
+        $this->amount_tva = $amount_tva;
+
+        return $this;
+    }
+
+    public function getTotalAmount(): ?float
+    {
+        return $this->total_amount;
+    }
+
+    public function setTotalAmount(float $total_amount): static
+    {
+        $this->total_amount = $total_amount;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,262 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    private ?int $street_number = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $street_address = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $postal_code = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $country = null;
+
+    #[ORM\Column]
+    private ?int $total_storage = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $registration_date = null;
+
+    #[ORM\OneToMany(targetEntity: Invoice::class, mappedBy: 'user_id', orphanRemoval: true)]
+    private Collection $number_invoices;
+
+    #[ORM\OneToMany(targetEntity: File::class, mappedBy: 'user_id', orphanRemoval: true)]
+    private Collection $number_file;
+
+    public function __construct()
+    {
+        $this->number_invoices = new ArrayCollection();
+        $this->number_file = new ArrayCollection();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getStreetNumber(): ?int
+    {
+        return $this->street_number;
+    }
+
+    public function setStreetNumber(int $street_number): static
+    {
+        $this->street_number = $street_number;
+
+        return $this;
+    }
+
+    public function getStreetAddress(): ?string
+    {
+        return $this->street_address;
+    }
+
+    public function setStreetAddress(string $street_address): static
+    {
+        $this->street_address = $street_address;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postal_code;
+    }
+
+    public function setPostalCode(string $postal_code): static
+    {
+        $this->postal_code = $postal_code;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getTotalStorage(): ?int
+    {
+        return $this->total_storage;
+    }
+
+    public function setTotalStorage(int $total_storage): static
+    {
+        $this->total_storage = $total_storage;
+
+        return $this;
+    }
+
+    public function getRegistrationDate(): ?\DateTimeInterface
+    {
+        return $this->registration_date;
+    }
+
+    public function setRegistrationDate(\DateTimeInterface $registration_date): static
+    {
+        $this->registration_date = $registration_date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Invoice>
+     */
+    public function getNumberInvoices(): Collection
+    {
+        return $this->number_invoices;
+    }
+
+    public function addNumberInvoice(Invoice $numberInvoice): static
+    {
+        if (!$this->number_invoices->contains($numberInvoice)) {
+            $this->number_invoices->add($numberInvoice);
+            $numberInvoice->setUserId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeNumberInvoice(Invoice $numberInvoice): static
+    {
+        if ($this->number_invoices->removeElement($numberInvoice)) {
+            // set the owning side to null (unless already changed)
+            if ($numberInvoice->getUserId() === $this) {
+                $numberInvoice->setUserId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, File>
+     */
+    public function getNumberFile(): Collection
+    {
+        return $this->number_file;
+    }
+
+    public function addNumberFile(File $numberFile): static
+    {
+        if (!$this->number_file->contains($numberFile)) {
+            $this->number_file->add($numberFile);
+            $numberFile->setUserId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeNumberFile(File $numberFile): static
+    {
+        if ($this->number_file->removeElement($numberFile)) {
+            // set the owning side to null (unless already changed)
+            if ($numberFile->getUserId() === $this) {
+                $numberFile->setUserId(null);
+            }
+        }
+
+        return $this;
+    }
+
+}
