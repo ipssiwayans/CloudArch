6b3cc6f83cbcacd4bea35bb51a5135121f47e368
samueltomen <113606314+samueltomen@users.noreply.github.com>
2024-03-10 22:35:09 +0100

    DEV-31: Ajout de la page d'erreur 404

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,6 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('app.html.twig', [
-            'controller_name' => 'HomeController',
         ]);
     }
 }
