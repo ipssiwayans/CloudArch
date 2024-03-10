1f69a1df1b6402c7a59cd6b07a37b91a12d3be4b
samueltomen <113606314+samueltomen@users.noreply.github.com>
2024-03-10 17:04:47 +0100

    update signup form & add INTL country librarie

--- src/Controller/AccessController.php
+++ src/Controller/AccessController.php
@@ -5,22 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Intl\Countries;
 
 class AccessController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function login(): Response
     {
         return $this->render('access/login.html.twig', [
-            'controller_name' => 'AccessController',
         ]);
     }
 
     #[Route('/signup', name: 'app_signup')]
     public function logout(): Response
     {
+        $countries = Countries::getNames();
         return $this->render('access/sign_up.html.twig', [
-            'controller_name' => 'AccessController',
+            'countries' => $countries,
         ]);
     }
 }
