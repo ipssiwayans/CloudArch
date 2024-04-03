abc95ce9cbd7ef05d22c737282d28edbec9889c9
Terosier Eddy <Eddyjohn.terosier@hotmail.com>
2024-04-03 15:17:36 +0200

    DEV-34: Suppression de l'image de l'utilisateur

--- src/Controller/AccessController.php
+++ src/Controller/AccessController.php
@@ -118,7 +118,6 @@ class AccessController extends AbstractController
                         $newFilename
                     );
                 } catch (FileException $e) {
-
                 }
 
                 $user->setImageFilename($newFilename);
