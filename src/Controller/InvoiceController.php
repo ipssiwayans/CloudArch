b183cce11e0bfc24294875fbfb615f32d7acef85
stomen <113606314+samueltomen@users.noreply.github.com>
2024-09-03 15:11:39 +0200

    DEV-43: add invoice page

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -2,10 +2,53 @@
 
 namespace App\Controller;
 
+use App\Manager\InvoiceManager;
+use App\Service\BreadcrumbService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/invoice', name: 'app_invoice')]
+#[Route('/invoice')]
 class InvoiceController extends AbstractController
 {
+    private BreadcrumbService $breadcrumbService;
+
+    public function __construct(BreadcrumbService $breadcrumbService)
+    {
+        $this->breadcrumbService = $breadcrumbService;
+    }
+
+    #[Route('/all', name: 'app_invoice_all')]
+    public function allInvoice(Session $session, InvoiceManager $invoiceManager): Response
+    {
+        $invoices = $invoiceManager->getInvoiceByUser($this->getUser());
+
+        $this->breadcrumbService->setSession($session);
+        $this->breadcrumbService->addBreadcrumb('app_invoice_all');
+
+        return $this->render('invoice/index.html.twig', [
+            'invoices' => $invoices,
+            'breadcrumbs' => $this->breadcrumbService->getBreadcrumbs(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_invoice_show')]
+    public function showInvoice(Session $session, InvoiceManager $invoiceManager, int $id): Response
+    {
+        $invoice = $invoiceManager->getInvoiceById($id);
+        $user = $this->getUser();
+
+        dump($invoice);
+        dump($user);
+        $this->breadcrumbService->setSession($session);
+        $this->breadcrumbService->addBreadcrumb('app_invoice_show');
+        $this->breadcrumbService->addBreadcrumb('Facture n°' . $id);
+
+        return $this->render('invoice/show.html.twig', [
+            'user' => $user,
+            'invoice' => $invoice,
+            'breadcrumbs' => $this->breadcrumbService->getBreadcrumbs(),
+        ]);
+    }
 }
