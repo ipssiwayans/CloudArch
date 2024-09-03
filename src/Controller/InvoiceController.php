b6b70f82c5e74527027481051b1568424a97c1f3
stomen <113606314+samueltomen@users.noreply.github.com>
2024-09-03 18:48:32 +0200

    DEV-49: download invoice

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use App\Manager\InvoiceManager;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Attribute\Route;
+use Twig\Environment;
 
 #[Route('/invoice')]
 class InvoiceController extends AbstractController
@@ -22,7 +26,7 @@ class InvoiceController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_invoice_show')]
-    public function showInvoice(Session $session, InvoiceManager $invoiceManager, int $id): Response
+    public function showInvoice(InvoiceManager $invoiceManager, int $id): Response
     {
         $invoice = $invoiceManager->getInvoiceById($id);
         $user = $this->getUser();
@@ -31,5 +35,38 @@ class InvoiceController extends AbstractController
             'user' => $user,
             'invoice' => $invoice,
         ]);
+    }
+
+    #[Route('/download/{id}', name: 'app_invoice_download')]
+    public function download(int $id, InvoiceManager $invoiceManager, Environment $twig): Response
+    {
+        $invoice = $invoiceManager->getInvoiceById($id);
+
+        if ($invoice->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $template = $twig->load('invoice/show.html.twig');
+        $html = $template->renderBlock('pdf_content', [
+            'invoice' => $invoice,
+            'user' => $this->getUser(),
+        ]);
+
+        $options = new Options();
+        $options->set('isHtml5ParserEnabled', true);
+        $options->set('isRemoteEnabled', true);
+
+        $dompdf = new Dompdf($options);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4');
+        $dompdf->render();
+
+        $pdfContent = $dompdf->output();
+
+        $response = new Response($pdfContent);
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Content-Disposition', ResponseHeaderBag::DISPOSITION_ATTACHMENT . '; filename="invoice_' . $id . '.pdf"');
+
+        return $response;
     }
 }
