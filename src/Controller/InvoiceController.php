acd70cd8b880136e170e10b17dbd83bc76f1e4eb
Terosier Eddy <Eddyjohn.terosier@hotmail.com>
2024-09-03 15:49:13 +0200

    DEV-000:hotfix breadcrumb part2

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Manager\InvoiceManager;
-use App\Service\BreadcrumbService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -12,24 +11,13 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/invoice')]
 class InvoiceController extends AbstractController
 {
-    private BreadcrumbService $breadcrumbService;
-
-    public function __construct(BreadcrumbService $breadcrumbService)
-    {
-        $this->breadcrumbService = $breadcrumbService;
-    }
-
     #[Route('/all', name: 'app_invoice_all')]
     public function allInvoice(Session $session, InvoiceManager $invoiceManager): Response
     {
         $invoices = $invoiceManager->getInvoiceByUser($this->getUser());
 
-        $this->breadcrumbService->setSession($session);
-        $this->breadcrumbService->addBreadcrumb('app_invoice_all');
-
         return $this->render('invoice/index.html.twig', [
             'invoices' => $invoices,
-            'breadcrumbs' => $this->breadcrumbService->getBreadcrumbs(),
         ]);
     }
 
@@ -39,14 +27,9 @@ class InvoiceController extends AbstractController
         $invoice = $invoiceManager->getInvoiceById($id);
         $user = $this->getUser();
 
-        $this->breadcrumbService->setSession($session);
-        $this->breadcrumbService->addBreadcrumb('app_invoice_show');
-        $this->breadcrumbService->addBreadcrumb('Facture n°' . $id);
-
         return $this->render('invoice/show.html.twig', [
             'user' => $user,
             'invoice' => $invoice,
-            'breadcrumbs' => $this->breadcrumbService->getBreadcrumbs(),
         ]);
     }
 }
